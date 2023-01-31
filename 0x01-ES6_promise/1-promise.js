573035fa696a7af7b2872dde05c5dd97fb75a79f
YohanesGetinet1 <johnget496@gmail.com>
2023-01-31 11:39:02 +0530

    Bug fixed

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,14 @@
-export default getFullResponseFromAPI(success){
- return new Promise((resolve, reject) => {
-  if (success) {
-   resolve ({
-	"status": 200,
-	"body": "Success"
-});
-}else (new Error(
-	'The fake API is not working currently',
-));
-}
-});
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error(
+        'The fake API is not working currently',
+      ));
+    }
+  });
 }
