dbf4a265efee3cf84b4ed6954152b83f695548cf
YohanesGetinet1 <johnget496@gmail.com>
2023-02-04 20:34:41 +0530

    Task 4 Namespace file

---
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,8 @@
+namespace Subjects{
+    export class Subject{
+        teacher : Subjects.Teacher;
+        set setTeacher(teacher : Subjects.Teacher){
+            this.teacher = teacher;
+        }
+    }
+}
