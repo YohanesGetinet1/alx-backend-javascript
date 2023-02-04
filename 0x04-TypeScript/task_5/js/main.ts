771fe151fee95aaa7d8924d985eb633fb0db153f
YohanesGetinet1 <johnget496@gmail.com>
2023-02-04 20:55:16 +0530

    Task 5 Brand convention and Nominal typing

---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,16 @@
+export interface MajorCredits{
+    credits : number;
+    brand : "Major";
+}
+
+export interface MinorCredits{
+    credits : number;
+    brand : "Minor";
+}
+
+export function sumMajorCredits(subject1 : MajorCredits, subject2 : MajorCredits) : MajorCredits{
+    return { credits : subject1.credits + subject2.credits , brand: "Major"};
+}
+ export function sumMinorCredits(subject1 : MinorCredits, subject2 : MinorCredits) : MinorCredits{
+    return { credits : subject1.credits + subject2.credits, brand: "Minor"};
+ }
