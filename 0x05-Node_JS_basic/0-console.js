b73160c4cb95348628ad03cfea7f2e290ecba549
YohanesGetinet1 <johnget496@gmail.com>
2023-04-02 18:11:21 +0530

    Basic Js with Node js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Yohanes Getinet <https://github.com/YohanesGetinet1>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
